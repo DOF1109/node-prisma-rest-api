2b369811ff6815722ff554d91c1c1d8b1e6c8abb
DOF1109 <friasdaniel.1109@gmail.com>
2024-08-02 11:04:50 -0300

    Obtengo los productos por API

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { PrismaClient } from '@prisma/client'
 
 const router = new Router();
+const prisma = new PrismaClient()
 
-router.get("/products", (req, res) => {
-  res.send("Products!");
+router.get("/products", async (req, res) => {
+  const products = await prisma.products.findMany()
+  res.json(products)
 });
 
 export default router;
